902616db65d2470d871d669126cd6cef710edb18
Maciej Łata <maciejcezarylata@gmail.com>
2021-05-26 23:05:13 +0200

    removing tasks

--- js/script.js
+++ js/script.js
@@ -10,20 +10,6 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-              ${task.content}
-            </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-    
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
@@ -32,22 +18,52 @@
         render();
     };
 
-const onFormSubmit = (event) => {
-    event.preventDefault();
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
 
-    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+    const render = () => {
+        let htmlString = "";
 
-    if(newTaskContent === "") {
-        return;
+        for (const task of tasks) {
+            htmlString += `
+            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+              ${task.content}
+              <button class="js-remove">usuń</button>
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove")
+
+        removeButtons.forEach((removeButton, taskIndex) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(taskIndex);
+            });
+        });
+    };
+
+   
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
     }
-
-   addNewTask(newTaskContent);
-}
 
     const init = () => {
         render();
 
-        const form=document.querySelector(".js-form");
+        const form = document.querySelector(".js-form");
 
         form.addEventListener("submit", onFormSubmit);
     };
