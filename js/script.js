05f968f3489b6a2f233acb85ef3b78adc21e1cea
Maciej Łata <maciejcezarylata@gmail.com>
2021-06-02 21:08:11 +0200

    button green fixed

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@
             <li
                 class="tasks__item js-task"
             >
-                <button class="tasks__button tasks__button-toggleDone js-toggleDone">
+                <button class="tasks__button tasks__button--toggleDone js-toggleDone">
                 ${task.done ? "✔" : " "}
                 </button>
                 <span class="tasks__content${task.done ? "tasks__content--done" : ""}">${task.content}</span>
